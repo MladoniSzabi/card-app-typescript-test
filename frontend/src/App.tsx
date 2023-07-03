4dd64aeb271c50015d3f82eba3a0b77de69b7411
Szabolcs Mladoniczky <szabimlado@gmail.com>
2023-07-03 13:48:50 +0100

    made the website switch to dark mode if the user selected dark mode or if the default color scheme is dark.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,35 @@
 import React from "react";
-import NavBar from './components/NavBar'
-import AllEntries from './routes/AllEntries'
-import NewEntry from './routes/NewEntry'
-import EditEntry from './routes/EditEntry'
-import { EntryProvider } from './utilities/globalContext'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AllEntries from "./routes/AllEntries";
+import EditEntry from "./routes/EditEntry";
+import NewEntry from "./routes/NewEntry";
+import { EntryProvider } from "./utilities/globalContext";
 
 export default function App() {
+  let isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (localStorage.getItem("isDarkMode") === "true") {
+    isDarkMode = true;
+  } else if (localStorage.getItem("isDarkMode") === "false") {
+    isDarkMode = false;
+  }
+
+  if (isDarkMode) {
+    document.querySelector("body")!.classList.add("dark");
+  }
+
   return (
-    <section>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+    <section className="dark:bg-slate-700 min-h-screen">
+      <Router>
+        <EntryProvider>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/" element={<AllEntries />}></Route>
+            <Route path="create" element={<NewEntry />}></Route>
+            <Route path="edit/:id" element={<EditEntry />}></Route>
+          </Routes>
+        </EntryProvider>
+      </Router>
     </section>
-    
   );
 }
